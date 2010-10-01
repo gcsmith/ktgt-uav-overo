ff25498ef81488c45321e3737d3d3d7cfef2a4dc
smith.garrett.c <smith.garrett.c@9a2b10f0-6bc6-460c-52f3-2a128a6cb42d>
2010-10-01 19:53:22 +0000

    include sys/stat.h

--- uav-control/uav_control.c
+++ uav-control/uav_control.c
@@ -4,6 +4,7 @@
 // -----------------------------------------------------------------------------
 
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/socket.h>
 #include <pthread.h>
 #include <netinet/in.h>
