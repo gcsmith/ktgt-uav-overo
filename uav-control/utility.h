c222c63008f3ab6d3c292572bf869d773aac466f
smith.garrett.c <smith.garrett.c@9a2b10f0-6bc6-460c-52f3-2a128a6cb42d>
2010-10-02 19:01:05 +0000

    add utility.h

---
+++ uav-control/utility.h
@@ -0,0 +1,14 @@
+// -----------------------------------------------------------------------------
+// Garrett Smith 2010
+// -----------------------------------------------------------------------------
+
+#ifndef _UAV_UTILITY_IMU__H_
+#define _UAV_UTILITY_IMU__H_
+
+#define MIN(a, b)   (((a) < (b)) ? (a) : (b))
+#define MAX(a, b)   (((a) > (b)) ? (a) : (b))
+#define LEN(x)      (sizeof(x) / sizeof((x)[0]))
+
+#endif // _UAV_UTILITY_IMU__H_
+
+
