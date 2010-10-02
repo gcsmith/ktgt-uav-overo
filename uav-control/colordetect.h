3b1e3fd7416bc80f7b4d2fdf68a478056951cadc
thetimr <thetimr@9a2b10f0-6bc6-460c-52f3-2a128a6cb42d>
2010-10-02 19:11:34 +0000

    cleaned colordetect.h

--- uav-control/colordetect.h
+++ uav-control/colordetect.h
@@ -28,51 +28,39 @@ typedef struct {
     char detected;
 } boxCoordinates;
 
-void runColorDetectionFile(const char * infilename, const char * outfilename, int quality,
-                        unsigned char R,
-                        unsigned char G, 
-                        unsigned char B, 
-                        short Ht, 
-                        short St, 
-                        short Lt);
+void runColorDetectionFile(const char * infilename, const char * outfilename,
+    int quality,
+    unsigned char R,unsigned char G, unsigned char B, 
+    short Ht, short St, short Lt);
 
-void runColorDetectionMemory(unsigned char * inbuffer, unsigned long * insize, int quality,
-                        unsigned char R,
-                        unsigned char G, 
-                        unsigned char B, 
-                        short Ht, 
-                        short St, 
-                        short Lt);
+void runColorDetectionMemory(unsigned char * inbuffer, unsigned long * insize,
+    int quality,
+    unsigned char R,unsigned char G, unsigned char B, 
+    short Ht, short St, short Lt);
                         
-void runColorDetection(unsigned char * RGBimage,int image_width, int image_height,
-                        unsigned char R,
-                        unsigned char G, 
-                        unsigned char B, 
-                        short Ht, 
-                        short St, 
-                        short Lt);
+void runColorDetection(unsigned char * RGBimage,
+    int image_width, int image_height,
+    unsigned char R,unsigned char G, unsigned char B, 
+    short Ht, short St, short Lt);
 
 
-void findColorRGB(unsigned char* RGBimage, int width, int height,unsigned char R, unsigned char G, unsigned char B, int threshold);
+void findColorRGB(unsigned char* RGBimage, int width, int height,
+    unsigned char R, unsigned char G, unsigned char B, int threshold);
+    
 void findColorHSL(short* HSLimage, int width, int height,
                 short H, short S, short L,
                 int Hthreshold, int Sthreshold, int Lthreshold,
                 unsigned char * RGBimage);
                 
 #ifdef DRAW_BOUNDING_BOX              
-void drawBoundingbox(unsigned char * RGBimage, int width, int height, int x1, int y1, int x2, int y2, int thickness ,
- unsigned char R, unsigned char G, unsigned char B);                
+void drawBoundingbox(unsigned char * RGBimage, int width, int height,
+    int x1, int y1, int x2, int y2, int thickness,
+    unsigned char R, unsigned char G, unsigned char B);                
 #endif                
 
-
-
-void COLORimageRGBtoHSL(unsigned char* COLORimage, short* HSLimage, int width, int height);
+void COLORimageRGBtoHSL(unsigned char* COLORimage, short* HSLimage,
+    int width, int height);
 
 void RGB2HSL (unsigned char R_in, unsigned char G_in, unsigned char B_in,
               short * h_out, short * s_out, short * l_out);
-
-double max_double(double x, double y);
-double min_double(double x, double y);
-
-
 
