982726fe0d676e8209a7181b2840dc57ef668a39
thetimr <thetimr@9a2b10f0-6bc6-460c-52f3-2a128a6cb42d>
2010-10-09 22:17:39 +0000

    made average test

--- sandbox/fixed_test.c
+++ sandbox/fixed_test.c
@@ -13,16 +13,61 @@
 
 
 int main(){
-    uint8_t r = 255;
-    uint8_t g = 0;
-    uint8_t b = 0;
+        
+    uint8_t r_f = 255;
+    uint8_t g_f = 0;
+    uint8_t b_f = 0;
+    
+    uint8_t r_d = 255;
+    uint8_t g_d = 0;
+    uint8_t b_d = 0;
+    
+    
+    double average_r = 0;
+    double average_g = 0;
+    double average_b = 0;
+    /*
+    double max_r = 0;
+    double max_b = 0;
+    double max_g = 0;
+    
+    double min_r = 255;
+    double min_b = 255;
+    double min_g = 255;
+    */
+    
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    
+    for(i = 0;i < 256; i++){
+        for(j = 0; j < 256; j++){
+            for(k = 0; k < 256; k++){
+                r_f = r_d = i;
+                g_f = g_d = j;
+                b_f = b_d = k;
+                
+                RGB2HSLfixed(&r_f,&g_f,&b_f);
+                RGB2HSL(&r_d,&g_d, &b_d);
+                
+                average_r += abs((double)r_f - (double)r_d);
+                average_g += abs((double)g_f - (double)g_d);
+                average_b += abs((double)b_f - (double)b_d);
+                
+                
+            
+            }
+        }
+    }
+        
+    average_r /= (255 * 255 * 255);    
+    average_g /= (255 * 255 * 255); 
+    average_b /= (255 * 255 * 255); 
+        
     
     
     
-    
-    RGB2HSLfixed(&r,&g,&b);
-    
-    printf("H: %d,S: %d,L: %d\n",r,g,b);
+    printf("r: %f,g: %f,b: %f\n",average_r,average_r,average_b);
     
     
 
