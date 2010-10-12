110314740a5ab3511c480d1467fe0b1986702efc
smith.garrett.c <smith.garrett.c@9a2b10f0-6bc6-460c-52f3-2a128a6cb42d>
2010-10-12 07:03:14 +0000

    add faster/less accurate division

--- uav-control/fp32.h
+++ uav-control/fp32.h
@@ -5,23 +5,17 @@
 
 #define FP32_BP  16
 #define FP32_BP2 32
+#define FP32_BPH 8
 
 #define FP32_FROM_INT(i)    ((int32_t)(i) << FP32_BP)
 #define FP32_TO_INT(f)      ((f) >> FP32_BP)
 #define FP32_DECIMALS(f)    ((f) & 0xff)
-#define FP32_MUL(x, y)      (int32_t)(((int64_t)(x) * (int64_t)(y)) >> FP32_BP)
+#define FP32_MUL(x, y)      (int32_t)((((x) >> FP32_BPH) * ((y) >> FP32_BPH)))
+#if 1
+#define FP32_DIV(x, y)      (int32_t)((((x) << 6) / ((y) >> 6)) << 4)
+#else
 #define FP32_DIV(x, y)      (int32_t)((((int64_t)(x) << FP32_BP2) / (y)) >> FP32_BP)
-
-// 16-bit fixed point
-
-#define FP16_BP  8
-#define FP16_BP2 16
-
-#define FP16_FROM_INT(i)    ((int16_t)(i) << FP16_BP)
-#define FP16_TO_INT(f)      ((f) >> FP16_BP)
-#define FP16_DECIMALS(f)    ((f) & 0xff)
-#define FP16_MUL(x, y)      (int16_t)(((int32_t)(x) * (int32_t)(y)) >> FP16_BP)
-#define FP16_DIV(x, y)      (int16_t)((((int32_t)(x) << FP16_BP2) / (y)) >> FP16_BP)
+#endif
 
 #endif // _UAV_FP32__H_
 
