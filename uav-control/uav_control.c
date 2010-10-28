61bfae127d71c8fae6d6cb70388b1204fdf2c5de
Kevin.Macksamie <Kevin.Macksamie@9a2b10f0-6bc6-460c-52f3-2a128a6cb42d>
2010-10-28 06:59:12 +0000

    server sends over defined IMU data in the angles array

--- uav-control/uav_control.c
+++ uav-control/uav_control.c
@@ -309,9 +309,9 @@ void run_server(imu_data_t *imu, const char *port)
                 cmd_buffer[PKT_LENGTH]  = PKT_VTI_LENGTH;
 
                 imu_read_angles(imu, angles, ACCESS_ASYNC);
-                memcpy(&cmd_buffer[PKT_VTI_YAW],   &angles[0], 4);
-                memcpy(&cmd_buffer[PKT_VTI_PITCH], &angles[1], 4);
-                memcpy(&cmd_buffer[PKT_VTI_ROLL],  &angles[2], 4);
+                memcpy(&cmd_buffer[PKT_VTI_YAW],   &angles[IMU_DATA_YAW], 4);
+                memcpy(&cmd_buffer[PKT_VTI_PITCH], &angles[IMU_DATA_PITCH], 4);
+                memcpy(&cmd_buffer[PKT_VTI_ROLL],  &angles[IMU_DATA_ROLL], 4);
                 
                 cmd_buffer[PKT_VTI_RSSI] = read_wlan_rssi(g_client.fd);
                 cmd_buffer[PKT_VTI_BATT] = read_vbatt();
